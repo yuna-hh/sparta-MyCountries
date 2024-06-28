a5704468aad63ae0a3059e2ab9eea924f035f5d8
yuna-hh <ghddbsk12@naver.com>
2024-06-28 11:03:03 +0900

    tailwindcss 재설치

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,12 +3,10 @@ import { Country } from "../types/country";
 
 export const getCountries = async (): Promise<Country[]> => {
   const response = await axios.get("https://restcountries.com/v3.1/all");
-  // console.log(response);
+  console.log(response);
   return response.data;
 };
 
 export interface AllInfo extends Country {
   isLike: boolean;
 }
-
-// console.log(getCountries);
